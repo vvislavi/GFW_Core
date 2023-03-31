f7288af87167e885e5c2f74f577db9cce40cb311
Vytautas V <vytautas.vislavicius@cern.ch>
2023-03-31 15:49:54 +0200

    Cleaned up, compiling with root, no warnings

--- AliGFW.cxx
+++ AliGFW.cxx
@@ -66,17 +66,11 @@ void AliGFW::AddRegion(string refName, Int_t lNhar, Int_t *lNparVec, Double_t lE
   lOneRegion.BitMask = BitMask; //Bit mask
   AddRegion(lOneRegion);
 };
-void AliGFW::SplitRegions() {
-  //Simple case. Will not look for overlaps, etc. Everything is left for end-used
-};
-
 Int_t AliGFW::CreateRegions() {
   if(fRegions.size()<1) {
     printf("No regions set. Skipping...\n");
     return 0;
   };
-  SplitRegions();
-  //for(auto pitr = fRegions.begin(); pitr!=fRegions.end(); pitr++) pitr->PrintStructure();
   Int_t nRegions=0;
   for(auto pItr=fRegions.begin(); pItr!=fRegions.end(); pItr++) {
     AliGFWCumulant *lCumulant = new AliGFWCumulant();
@@ -96,7 +90,7 @@ void AliGFW::Fill(Double_t eta, Int_t ptin, Double_t phi, Double_t weight, Int_t
   if(!fInitialized) return;
   for(Int_t i=0;i<(Int_t)fRegions.size();++i) {
     if(fRegions.at(i).EtaMin<eta && fRegions.at(i).EtaMax>eta && (fRegions.at(i).BitMask&mask))
-      fCumulants.at(i).FillArray(eta,ptin,phi,weight,SecondWeight);
+      fCumulants.at(i).FillArray(ptin,phi,weight,SecondWeight);
   };
 };
 complex<Double_t> AliGFW::TwoRec(Int_t n1, Int_t n2, Int_t p1, Int_t p2, Int_t ptbin, AliGFWCumulant *r1, AliGFWCumulant *r2, AliGFWCumulant *r3) {
@@ -297,12 +291,10 @@ void AliGFW::s_replace_all(string &instr, const string &pattern1, const string &
   while(lpos>-1) { s_replace(instr,pattern1,pattern2,lpos); lpos=s_index(instr,pattern1,lpos); };
 };
 Bool_t AliGFW::s_tokenize(string &instr, string &subs, Int_t &spos, const string &delim) {
-  if(spos<0 || spos>=instr.size()) {spos=-1; subs=""; return kFALSE;};
+  if(spos<0 || spos>=(Int_t)instr.size()) {spos=-1; subs=""; return kFALSE;};
   Int_t lpos = s_index(instr,delim,spos);
   if(lpos<0) lpos=instr.size();
   subs = instr.substr(spos,lpos-spos);
   spos=lpos+1;
   return kTRUE;
-  //2 -2
-  //0123 ->4
 }
